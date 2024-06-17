4d7946e341b6412b40b027f88e34b8d5e3ed581f
Leon Sering <leon@sering.eu>
2024-06-17 23:27:23 +0200

    moved main.rs to tsp/

--- src/examples/mod.rs
+++ src/examples/mod.rs
@@ -1,3 +1,3 @@
-//! This module contains the implementation of the [TSP][tsp] and several
+//! This module contains an example implementation of the [TSP][tsp] and several
 //! [solvers][tsp::solvers].
 pub mod tsp;
